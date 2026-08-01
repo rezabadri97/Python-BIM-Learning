2064693b969d76b0bcf6103531b59f79f53f0663
Rezabadri97i <Rezabadri97@gmail.com>
2026-08-01 05:58:52 +0330

    feat: scan BIM project folder with pathlib iterdir

---
+++ pathlib_os/exercise_07_project_folder_scanner.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+folder_path=Path("D:/Python BIM Learning/Warehouse_01")
+
+for item in folder_path.iterdir():
+    if item.is_dir:
+        print(f"Folder Name: {item.name}")
+    elif item.is_file:
+        print(f"File Name: {item.name}")
